45c1698667b84a534bd031569851ce0af576531d
WiOS Gaurav H. Parmar <GauravHP@ProjectDemo.biz>
2018-06-08 18:23:01 +0530

    changes in the download script

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -ev
 
+rm -fr Frameworks/DiyalogEngine.framework
+
 echo "fetching DiyalogEngine.framework"
 curl -OL http://diyalog.im/app/pkgs/DiyalogEngine.zip
 
 unzip -o -q DiyalogEngine.zip
+mv DiyalogEngine.framework Frameworks
+mv DiyalogEngine.framework.dSYM Frameworks
 rm DiyalogEngine.zip
